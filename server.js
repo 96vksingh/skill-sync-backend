474f86088b5351c73109a5d763463ad4691ae178
Vishal Kumar Singh <vishal.k@zerozilla.com>
2025-07-28 16:01:33 +0530

    updateed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 //   credentials: true
 // }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'https://skill-sync-one-tan.vercel.app',
   credentials: true
 }));
 
